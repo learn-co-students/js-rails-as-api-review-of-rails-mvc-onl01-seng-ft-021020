0634df6c73c978c1ce260c5cebe381e5a8a825c4
reddevilcero <reddevil_cero@hotmail.com>
2020-05-12 10:16:17 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Add route from Readme
+  get '/birds' => 'birds#index'
 end
--- db/migrate/2019_create_birds.rb
+++ db/migrate/2019_create_birds.rb
@@ -1,1 +1,13 @@
+# frozen_string_literal: true
+
 # Add code from Readme
+class CreateBirds < ActiveRecord::Migration[5.2]
+  def change
+    create_table :birds do |t|
+      t.string :name
+      t.string :species
+
+      t.timestamps
+    end
+  end
+end
